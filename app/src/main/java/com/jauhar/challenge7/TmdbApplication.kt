337fe53fcc714e977ee799d34b4bbcc1dc949d64
jauharzuhdhan <jauharzuhdhan@gmail.com>
2022-12-30 23:34:00 +0700

    fixing challenge 8

---
+++ app/src/main/java/com/jauhar/challenge7/TmdbApplication.kt
@@ -0,0 +1,10 @@
+@file:Suppress("RemoveEmptyClassBody")
+
+package com.jauhar.challenge7
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class TmdbApplication : Application() {
+}
